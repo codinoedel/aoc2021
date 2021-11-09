7f447be6a5d565fd36fc8f58caacc089fa17cc62
Codi Noedel <codi.noedel@gmail.com>
2021-11-08 17:22:17 -0800

    remove generated file

--- src/1.js
+++
@@ -1,6 +0,0 @@
-var inputFileName = './1.input';
-return stat(inputFileName).then(function (err, stats) { return (open(inputFileName).then(function (err, fd) { return function (, buff, read) {
-    if (buff === void 0) { buff = Buffer.alloc(stats.size); }
-    return ;
-}; }, (fd, buff).then(function (err) { return (console.log('input', buff.toString())); }))); });
-export {};
